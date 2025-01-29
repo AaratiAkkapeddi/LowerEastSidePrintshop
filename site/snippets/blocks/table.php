79397b636a87caf6384885a811b6d34e194c55f8
Aarati Akkapeddi <aarati@Mac.home>
2025-01-29 18:42:08 -0500

    publishing artists

--- site/snippets/blocks/table.php
+++ site/snippets/blocks/table.php
@@ -12,7 +12,21 @@ $rows = $block ->rows()->toStructure();
 <path d="M13.2856 1.94116L7.23811 8.44336L1.22068 1.94116" stroke="#E03C31" stroke-width="2" stroke-linecap="round"/>
 </svg>
 </summary>
+<?php if( $row -> artist()):?>
+
+    <?php $artists = $row-> artists() -> toPages() ?>
+    <?php foreach( $artists as $artist): ?>
+        <li>
+         <a  class="artist-name" href="<?= $artist -> url() ?>"> <?= $artist -> title() ?></a><br>
+         <a href="<?= $artist -> url() ?>" style="background-image: url('<?= $artist -> main_img() -> toFile() -> url() ?>');" class="artist-image-card"></a>
+        </li>
+
+
+    <?php endforeach ?>
+
+<? else: ?>
        <div> <?= $row-> description() ?></div>
+<?php endif; ?>
     </details>
 
   <?php endforeach ?>
